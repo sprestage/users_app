bed4a34e32ef4bd4a96f35cb9f10dd1ec0dafaf3
Susan Prestage <susan.prestage@gmail.com>
2013-09-08 22:29:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# CodeFellows final application, step 3:
+
+Generate a Rails app that allows a user to create, edit, update, and destroy (delete) users. Users should have first name, last name, and email address fields. Deploy this app to Heroku (a free *.herokuapp.com URL) so we can test it out live.
+
+If you have time, add extra features to this app (be creative) and tell us what you did so we don’t miss it.
+
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+UsersApp::Application.config.secret_key_base = '350a93002cd18ca1b64ad6672c0694e2bc0c34a5d2488ce94c0adf93babc0ce73c14d132add9a46c73460582577d4f5bc620875e75af910ce9b19c1b5d3e84b9'
